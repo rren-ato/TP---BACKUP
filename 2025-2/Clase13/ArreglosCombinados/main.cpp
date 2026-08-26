ee7a2afba92132f410937b8b7fcd9302efc056e1
rren-ato <renato.ruiz@pucp.edu.pe>
2026-08-25 19:45:36 -0500

    Add files via upload

---
+++ 2025-2/Clase13/ArreglosCombinados/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Erasmo Gomez
+ * Description: 
+ * Created on April 30, 2025, 8:18 PM
+ */
+
+#include "funciones.hpp"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    calcular_arreglo_combinado("RegistroNum1.txt", "RegistroNum2.txt", "Reporte.txt");
+    return 0;
+}
+
